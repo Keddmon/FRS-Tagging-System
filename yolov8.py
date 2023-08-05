86c5661385ff229969b7df5a5566b4b064d572c0
Keddmon <dh03219@naver.com>
2023-08-05 17:32:13 +0900

    feat. Add savedImage(openpose)

---
+++ yolov8.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+
+# # 모델 로드
+# model = YOLO('yolov8n.yaml')  # build a new model from YAML
+# model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+# model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+
+# # 모델 학습
+# model.train(data='C:\\FTS\\halflabeling\\dataset.yaml', epochs=100, imgsz=640)
+
+
+# 학습 모델
+model = YOLO('C:\\FTS\\runs\\detect\\train4\\weights\\best.pt')
+
+# 이미지 폴더 위치
+source = 'C:\\project_images\\images\\americancasual_img'
+
+# 결과 이미지
+results = model.predict(source, save=True)  # save=true =   
+
+
+
+# 모델 로드
+# model = YOLO('C:\\FRS\\runs\\detect\\train\\weights\\best.pt')  # pretrained YOLOv8n model
